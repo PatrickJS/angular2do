24b273339cd749d5fe1180d506fcf4ee7ea18056
gdi2290 <github@gdi2290.com>
2015-02-09 03:25:16 -0800

    refactor(todo-app): convert into components

--- app/components/todo-app/todo-app.js
+++ app/components/todo-app/todo-app.js
@@ -1,193 +1,57 @@
-import {
-  bootstrap,
-  Component,
-  Decorator,
-  TemplateConfig,
-  Foreach,
-  NgElement
-} from 'angular2/angular2';
-import {DOM} from 'angular2/src/facade/dom';
-import {Injector, Inject, bind} from 'angular2/di';
-import {AngularFire, FirebaseArray} from 'firebase/AngularFire';
+import {Component, TemplateConfig, NgElement} from 'angular2/angular2';
 
-var keymap = {
-  tab: 9,
-  enter: 13,
-  esc: 27,
-  up: 38,
-  down: 40
-};
+import {TodoHeader} from 'components/todo-header/todo-header.js';
+import {TodoMain} from 'components/todo-main/todo-main.js';
+import {TodoFooter} from 'components/todo-footer/todo-footer.js';
 
-@Decorator({
-  selector: '[todo-focus]',
-  bind: {
-    'todo-focus': 'isFocused'
-  }
-})
-class TodoFocus {
-  set isFocused(value) {
-    console.log('isFocused', value);
-    if (value) {
-      this.element.domElement.focus();
-    }
-    return value;
-  }
-  constructor(el: NgElement) {
-    console.log('todo-focus', el);
-    this.element = el;
+import {Style} from 'decorators/style.js';
 
-  }
-}
-
-@Decorator({
-  selector: '[ng-show]',
-  bind: {
-    'ng-show': 'ngShow'
-  }
-})
-class NgShow {
-  element:NgElement;
-  set ngShow(value) {
-    var el = this.element.domElement;
-    if (value) {
-      el.style.display = 'block';
-      DOM.removeClass(el, 'hidden');
-      DOM.addClass(el, 'visible');
-    } else {
-      el.style.display = 'none';
-      DOM.removeClass(el, 'visible');
-      DOM.addClass(el, 'hidden');
-    }
-    return value;
-  }
-  constructor(el: NgElement) {
-    console.log('ng-show', el);
-    this.element = el;
-  }
-}
-
-@Decorator({
-  selector: '[ng-hide]',
-  bind: {
-    'ng-hide': 'ngHide'
-  }
-})
-class NgHide {
-  element:NgElement;
-  set ngHide(value) {
-    var el = this.element.domElement;
-    if (value) {
-      el.style.display = 'none';
-      DOM.removeClass(el, 'visible');
-      DOM.addClass(el, 'hidden');
-    } else {
-      el.style.display = 'block';
-      DOM.removeClass(el, 'hidden');
-      DOM.addClass(el, 'visible');
-    }
-    return value;
-  }
-  constructor(el: NgElement) {
-    console.log('ng-hide', el);
-    this.element = el;
-  }
-  toggleClass() {
-    this.element.domElement
-
-  }
-  toggleVisible
-}
 
 @Component({
   selector: 'todo-app',
-  componentServices: [
-    AngularFire,
-    bind(Firebase).toValue(new Firebase('https://webapi.firebaseio.com/test'))
-  ],
   template: new TemplateConfig({
-    url: 'components/todo-app/todo-app.html',
+    url: 'app/components/todo-app/todo-app.html',
     directives: [
-      Foreach,
-      TodoFocus,
-      NgShow,
-      NgHide
+      TodoHeader,
+      TodoMain,
+      TodoFooter,
+      Style
     ]
   })
 })
-export default class TodoApp {
-  text: string;
-  todoService: FirebaseArray;
-  todoEdit: any;
+export class TodoApp {
 
-  constructor(sync: AngularFire) {
+  constructor() {
     // TODO: refactor into TodoStorage service
-    this.todoService = sync.asArray();
-    this.text = '';
-    this.todoEdit = null;
-  }
-  enterTodo($event) {
-    this.text = $event.target.value;
-    if ($event.which === keymap.enter) { // ENTER_KEY
-      this.addTodo();
-    }
-  }
-  editTodo($event, todo) {
-    this.todoEdit = todo;
-  }
-  doneEditing($event, todo) {
-    var which = $event.which;
-    var target = $event.target;
-    if (which === keymap.enter) {
-      todo.title = target.value;
-      this.todoService.save(todo);
-      this.todoEdit = null;
-    }
-    else if (which === keymap.esc) {
-      this.todoEdit = null;
-      target.value = todo.title;
-    }
-  }
-  addTodo() {
-    this.todoService.add({
-      title: this.text,
-      completed: false
-    });
-    this.text = '';
-  }
-  completeMe(todo) {
-    todo.completed = !todo.completed;
-    this.todoService.save(todo);
-  }
-  deleteMe(todo) {
-    this.todoService.remove(todo);
-  }
-  toggleAll($event) {
-    var isComplete = $event.target.checked;
-    this.todoService.list.forEach((todo) => {
-      todo.completed = isComplete;
-      this.todoService.save(todo);
-    });
-  }
-  clearCompleted() {
-    var toClear = {};
-    this.todoService.list.forEach((todo) => {
-      if (todo.completed) {
-        toClear[todo._key] = null;
+    this.styles = {
+      title: {
+        height: '130px',
+        margin: '0',
+        lineHeight: '1.4em',
+        width: '100%',
+        fontSize: '100px',
+        fontWeight: '100',
+        textAlign: 'center',
+        color: 'rgba(175, 47, 47, 0.15)',
+        textRendering: 'optimizeLegibility'
+      },
+      section: {
+        background: '#fff',
+        margin: '0 0 40px 0',
+        position: 'relative',
+        boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1)'
+      },
+      footer: {
+        margin: '65px auto 0',
+        color: '#bfbfbf',
+        fontSize: '10px',
+        textShadow: '0 1px 0 rgba(255, 255, 255, 0.5)',
+        textAlign: 'center'
       }
-    });
-    this.todoService.bulkUpdate(toClear);
+    }; // end styles
+
   }
-  get remainingCount() {
-    return this.todoService.list.filter((todo) => !todo.completed).length;
-  }
-  get completedCount() {
-    return this.todoService.list.filter((todo) => todo.completed).length;
-  }
-  get locationPath() {
-    // dirty checking plz
-    // TODO: refactor into service
-    return location.hash.replace('#/', '');
-  }
+
 }
 
 
