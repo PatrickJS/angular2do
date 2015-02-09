b35012b28facf9c95b67d421a9c9780dd0bc1264
gdi2290 <github@gdi2290.com>
2015-02-09 03:29:30 -0800

    feat: todo-footer

---
+++ app/components/todo-footer/todo-footer.js
@@ -0,0 +1,77 @@
+import {Component, TemplateConfig} from 'angular2/angular2';
+import {bind} from 'angular2/di';
+import {AngularFire, FirebaseArray} from 'firebase/AngularFire';
+
+import {NgShow} from 'decorators/ng-show.js';
+import {NgHide} from 'decorators/ng-hide.js';
+import {Style} from 'decorators/style.js';
+
+@Component({
+  selector: 'todo-footer',
+  componentServices: [
+    AngularFire,
+    bind(Firebase).toValue(new Firebase('https://webapi.firebaseio.com/test'))
+  ],
+  template: new TemplateConfig({
+    url: 'app/components/todo-footer/todo-footer.html',
+    directives: [
+      NgShow,
+      NgHide,
+      Style
+    ]
+  })
+})
+export class TodoFooter {
+  todoService: FirebaseArray;
+  todoEdit: any;
+  constructor(sync: AngularFire) {
+    this.todoService = sync.asArray();
+    this.todoEdit = null;
+    this.styles = {
+      footer: {
+        color: '#777',
+        padding: '10px 15px',
+        height: '20px',
+        textAlign: 'center',
+        borderTop: '1px solid #e6e6e6'
+      },
+      count: {
+        float: 'left',
+        textAlign: 'left'
+      },
+      countString: {
+        fontWeight: '300'
+      },
+      filters: {
+        margin: '0',
+        padding: '0',
+        listStyle: 'none',
+        position: 'absolute',
+        right: '0',
+        left: '0'
+      }
+    }; // end styles
+  }
+  clearCompleted() {
+    var toClear = {};
+    this.todoService.list.forEach((todo) => {
+      if (todo.completed) {
+        toClear[todo._key] = null;
+      }
+    });
+    this.todoService.bulkUpdate(toClear);
+  }
+  get remainingCount() {
+    return this.todoService.list.filter((todo) => !todo.completed).length;
+  }
+  get completedCount() {
+    return this.todoService.list.filter((todo) => todo.completed).length;
+  }
+  get locationPath() {
+    // dirty checking plz
+    // TODO: refactor into service
+    return location.hash.replace('#/', '');
+  }
+}
+
+
