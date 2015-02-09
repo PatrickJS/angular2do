9de1bb3db9f575dea26e851257c32048d0406302
gdi2290 <github@gdi2290.com>
2015-02-09 03:43:42 -0800

    chore: include angular2 source

---
+++ deps/angular2/angular2/src/core/compiler/interfaces.js
@@ -0,0 +1,20 @@
+System.register("angular2/src/core/compiler/interfaces", [], function($__export) {
+  "use strict";
+  var __moduleName = "angular2/src/core/compiler/interfaces";
+  var OnChange;
+  return {
+    setters: [],
+    execute: function() {
+      OnChange = $__export("OnChange", (function() {
+        var OnChange = function OnChange() {};
+        return ($traceurRuntime.createClass)(OnChange, {onChange: function(changes) {
+            throw "OnChange.onChange is not implemented";
+          }}, {});
+      }()));
+    }
+  };
+});
+
+//# sourceMappingURL=/Users/patrick/Documents/open source/angular/modules/angular2/src/core/compiler/interfaces.map
+
+//# sourceMappingURL=./interfaces.map
