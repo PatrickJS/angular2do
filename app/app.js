5327b114fbdcadbcee87f1b043b38294897ba89f
gdi2290 <github@gdi2290.com>
2015-02-09 03:23:38 -0800

    fix(systemjs): path

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 import {bootstrap} from 'angular2/angular2';
-import TodoApp from './components/todo-app/todo-app.js';
+import {TodoApp} from 'components/todo-app/todo-app.js';
 
 export function main() {
   bootstrap(TodoApp);
