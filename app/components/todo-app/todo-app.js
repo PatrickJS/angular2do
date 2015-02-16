19772590abb8eede6ce1abab1a049e2ebe27a3bc
gdi2290 <github@gdi2290.com>
2015-02-16 04:58:05 -0800

    refactor(todo-app): styles in template

--- app/components/todo-app/todo-app.js
+++ app/components/todo-app/todo-app.js
@@ -4,8 +4,6 @@ import {TodoHeader} from 'components/todo-header/todo-header.js';
 import {TodoMain} from 'components/todo-main/todo-main.js';
 import {TodoFooter} from 'components/todo-footer/todo-footer.js';
 
-import {Style} from 'decorators/style.js';
-
 
 @Component({
   selector: 'todo-app'
@@ -21,33 +19,6 @@ import {Style} from 'decorators/style.js';
 export class TodoApp {
 
   constructor() {
-    // TODO: refactor into TodoStorage service
-    this.styles = {
-      title: {
-        height: '130px',
-        margin: '0',
-        lineHeight: '1.4em',
-        width: '100%',
-        fontSize: '100px',
-        fontWeight: '100',
-        textAlign: 'center',
-        color: 'rgba(175, 47, 47, 0.15)',
-        textRendering: 'optimizeLegibility'
-      },
-      section: {
-        background: '#fff',
-        margin: '0 0 40px 0',
-        position: 'relative',
-        boxShadow: '0 2px 4px 0 rgba(0, 0, 0, 0.2), 0 25px 50px 0 rgba(0, 0, 0, 0.1)'
-      },
-      footer: {
-        margin: '65px auto 0',
-        color: '#bfbfbf',
-        fontSize: '10px',
-        textShadow: '0 1px 0 rgba(255, 255, 255, 0.5)',
-        textAlign: 'center'
-      }
-    }; // end styles
 
   }
 
